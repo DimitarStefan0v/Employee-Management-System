d7e23ef32710eb0065424d5ef760b544fa585bd9
Dimitar Stefanov <dimitarnew9@gmail.com>
2023-03-09 18:47:56 +0200

    Add EmployeesListViewModel and EmployeeInListViewMode

---
+++ src/Web/EMS.Web.ViewModels/Employees/EmployeeInListViewModel.cs
@@ -0,0 +1,24 @@
+﻿namespace EMS.Web.ViewModels.Employees
+{
+    using System;
+
+    using EMS.Data.Models;
+    using EMS.Services.Mapping;
+
+    public class EmployeeInListViewModel : IMapFrom<Employee>
+    {
+        public string FirstName { get; set; }
+
+        public string LastName { get; set; }
+
+        public string Email { get; set; }
+
+        public string PhoneNumber { get; set; }
+
+        public DateTime DateOfBirth { get; set; }
+
+        public double MonthlySalary { get; set; }
+
+        public string AddedByUserUserName { get; set; }
+    }
+}
---
+++ src/Web/EMS.Web.ViewModels/Employees/EmployeesListViewModel.cs
@@ -0,0 +1,9 @@
+﻿namespace EMS.Web.ViewModels.Employees
+{
+    using System.Collections.Generic;
+
+    public class EmployeesListViewModel
+    {
+        public IEnumerable<EmployeeInListViewModel> Employees { get; set; }
+    }
+}
