694f0851fa7476acb73a814f553e5c67602cc649
Dimitar Stefanov <dimitarnew9@gmail.com>
2023-03-09 12:40:15 +0200

    Add EmployeesService and interface and register it

---
+++ src/Services/EMS.Services.Data/EmployeesService.cs
@@ -0,0 +1,38 @@
+﻿namespace EMS.Services.Data
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Text;
+    using System.Threading.Tasks;
+
+    using EMS.Data.Common.Repositories;
+    using EMS.Data.Models;
+    using EMS.Web.ViewModels.Employees;
+
+    public class EmployeesService : IEmployeesService
+    {
+        private readonly IDeletableEntityRepository<Employee> employeesRepository;
+
+        public EmployeesService(IDeletableEntityRepository<Employee> employeesRepository)
+        {
+            this.employeesRepository = employeesRepository;
+        }
+
+        public async Task CreateAsync(CreateEmployeeInputModel input)
+        {
+            var employee = new Employee
+            {
+                FirstName = input.FirstName,
+                LastName = input.LastName,
+                Email = input.Email,
+                PhoneNumber = input.PhoneNumber,
+                DateOfBirth = input.DateOfBirth,
+                MonthlySalary = input.MonthlySalary,
+            };
+
+            await this.employeesRepository.AddAsync(employee);
+            await this.employeesRepository.SaveChangesAsync();
+        }
+    }
+}
---
+++ src/Services/EMS.Services.Data/IEmployeesService.cs
@@ -0,0 +1,11 @@
+﻿namespace EMS.Services.Data
+{
+    using System.Threading.Tasks;
+
+    using EMS.Web.ViewModels.Employees;
+
+    public interface IEmployeesService
+    {
+        Task CreateAsync(CreateEmployeeInputModel input);
+    }
+}
