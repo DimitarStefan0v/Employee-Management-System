a3cd9d5ba3bde68c289f8ff46d1c49108f69834f
Dimitar Stefanov <dimitarnew9@gmail.com>
2023-03-09 22:12:49 +0200

    Add abstract BaseEmployeeInputModel

---
+++ src/Web/EMS.Web.ViewModels/Employees/BaseEmployeeInputModel.cs
@@ -0,0 +1,44 @@
+﻿namespace EMS.Web.ViewModels.Employees
+{
+    using System;
+    using System.ComponentModel.DataAnnotations;
+
+    using EMS.Common.ErrorMessages;
+    using EMS.Common.ValidationAttributes;
+
+    public abstract class BaseEmployeeInputModel
+    {
+        [Display(Name = EmployeeInputErrorMessages.DisplayFirstName)]
+        [Required(ErrorMessage = EmployeeInputErrorMessages.FirstNameRequired)]
+        [MinLength(2, ErrorMessage = EmployeeInputErrorMessages.FirstNameMinLength)]
+        [MaxLength(50, ErrorMessage = EmployeeInputErrorMessages.FirstNameMaxLength)]
+        public string FirstName { get; set; }
+
+        [Display(Name = EmployeeInputErrorMessages.DisplayLastName)]
+        [Required(ErrorMessage = EmployeeInputErrorMessages.LastNameRequired)]
+        [MinLength(2, ErrorMessage = EmployeeInputErrorMessages.LastNameMinLength)]
+        [MaxLength(50, ErrorMessage = EmployeeInputErrorMessages.LastNameMaxLength)]
+        public string LastName { get; set; }
+
+        [Display(Name = EmployeeInputErrorMessages.DisplayEmail)]
+        [Required(ErrorMessage = EmployeeInputErrorMessages.EmailRequired)]
+        [EmailAddress(ErrorMessage = EmployeeInputErrorMessages.EmailValidation)]
+        public string Email { get; set; }
+
+        [Display(Name = EmployeeInputErrorMessages.DisplayPhoneNumber)]
+        [Required(ErrorMessage = EmployeeInputErrorMessages.PhoneNumberRequired)]
+        [DataType(DataType.PhoneNumber, ErrorMessage = EmployeeInputErrorMessages.PhoneNumberValidation)]
+        public string PhoneNumber { get; set; }
+
+        [Display(Name = EmployeeInputErrorMessages.DisplayDateOfBirth)]
+        [Required(ErrorMessage = EmployeeInputErrorMessages.DateOfBirthRequired)]
+        [DataType(DataType.Date, ErrorMessage = EmployeeInputErrorMessages.DateOfBirthRequired)]
+        [CurrentYearMaxValueAttribute(1900)]
+        public DateTime DateOfBirth { get; set; }
+
+        [Display(Name = EmployeeInputErrorMessages.DisplayMonthlySalary)]
+        [Required(ErrorMessage = EmployeeInputErrorMessages.MonthlySalaryRequired)]
+        [Range(600, 20000, ErrorMessage = EmployeeInputErrorMessages.MonthlySalaryRange)]
+        public double MonthlySalary { get; set; }
+    }
+}
