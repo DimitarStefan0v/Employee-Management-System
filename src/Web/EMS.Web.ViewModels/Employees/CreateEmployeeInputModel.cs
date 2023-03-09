42c474d5cf222537d798a851435c0a2f93f9ec06
Dimitar Stefanov <dimitarnew9@gmail.com>
2023-03-09 12:30:54 +0200

    Add CreateEmployeeInputModel

---
+++ src/Web/EMS.Web.ViewModels/Employees/CreateEmployeeInputModel.cs
@@ -0,0 +1,19 @@
+﻿namespace EMS.Web.ViewModels.Employees
+{
+    using System;
+
+    public class CreateEmployeeInputModel
+    {
+        public string FirstName { get; set; }
+
+        public string LastName { get; set; }
+
+        public string Email { get; set; }
+
+        public string PhoneNumber { get; set; }
+
+        public DateTime DateOfBirth { get; set; }
+
+        public double MonthlySalary { get; set; }
+    }
+}
