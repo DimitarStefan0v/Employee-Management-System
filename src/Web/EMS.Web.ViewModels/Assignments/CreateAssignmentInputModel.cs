a21becb3ec1914a2d9298619be1ad039726ea57d
Dimitar Stefanov <dimitarnew9@gmail.com>
2023-03-11 12:47:42 +0200

    Add abstract BaseAssignmentInputModel and EditAssignmentInputModel

--- src/Web/EMS.Web.ViewModels/Assignments/CreateAssignmentInputModel.cs
+++ src/Web/EMS.Web.ViewModels/Assignments/CreateAssignmentInputModel.cs
@@ -1,32 +1,6 @@
 ﻿namespace EMS.Web.ViewModels.Assignments
 {
-    using System;
-    using System.ComponentModel.DataAnnotations;
-
-    using EMS.Common.ErrorMessages;
-    using EMS.Common.ValidationAttributes;
-
-    public class CreateAssignmentInputModel
+    public class CreateAssignmentInputModel : BaseAssignmentInputModel
     {
-        [Required(ErrorMessage = AssignmentInputErrorMessages.TitleRequired)]
-        [MinLength(4, ErrorMessage = AssignmentInputErrorMessages.TitleMinLength)]
-        [MaxLength(50, ErrorMessage = AssignmentInputErrorMessages.TitleMaxLength)]
-        public string Title { get; set; }
-
-        [Required(ErrorMessage = AssignmentInputErrorMessages.DescriptionRequired)]
-        [MinLength(8, ErrorMessage = AssignmentInputErrorMessages.DescriptionMinLength)]
-        [MaxLength(500, ErrorMessage = AssignmentInputErrorMessages.DescriptionMaxLength)]
-        public string Description { get; set; }
-
-        [Display(Name = AssignmentInputErrorMessages.DisplayStartDate)]
-        [Required(ErrorMessage = AssignmentInputErrorMessages.StartDateRequired)]
-        [DataType(DataType.Date, ErrorMessage = AssignmentInputErrorMessages.StartDateRequired)]
-        [CurrentYearMaxValueAttribute(2015)]
-        public DateTime StartDate { get; set; }
-
-        [Display(Name = AssignmentInputErrorMessages.DisplayDueDate)]
-        [Required(ErrorMessage = AssignmentInputErrorMessages.DueDateRequired)]
-        [DataType(DataType.Date, ErrorMessage = AssignmentInputErrorMessages.DueDateRequired)]
-        public DateTime DueDate { get; set; }
     }
 }
---
+++ src/Web/EMS.Web.ViewModels/Assignments/EditAssignmentInputModel.cs
@@ -0,0 +1,9 @@
+﻿namespace EMS.Web.ViewModels.Assignments
+{
+    using EMS.Data.Models;
+    using EMS.Services.Mapping;
+
+    public class EditAssignmentInputModel : BaseAssignmentInputModel, IMapFrom<Assignment>
+    {
+    }
+}
